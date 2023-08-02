cf2322742e14e9d62a3167a735606da17710ca1d
Mitch L Newman <mitchellnewman3@gmail.com>
2023-08-02 16:13:05 +0100

    Getting Started with Javascript exercises

---
+++ GettingStarted.js
@@ -0,0 +1,11 @@
+console.log("Mitch");
+console.info("Newman");
+console.warn("The UK");
+console.error("Taurus");
+
+let Car = "Lexus";
+let Model = "LFA";
+console.log(`My favourite car is ${Car} and the model is ${Model}`);
+
+
+console.log("This is now %c the end of the exercises.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
