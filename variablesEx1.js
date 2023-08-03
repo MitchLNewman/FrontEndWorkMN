39922238a8b2418fb84a72c9277f3ad7dfaa17cc
Mitch L Newman <mitchellnewman3@gmail.com>
2023-08-03 10:15:10 +0100

    JavaScript Variables Exercise

---
+++ variablesEx1.js
@@ -0,0 +1,11 @@
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+typeof(a);
+typeof(b);
+typeof(c);
+typeof(d);
+typeof(e);
---
+++ variablesEx2.js
@@ -0,0 +1,5 @@
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
